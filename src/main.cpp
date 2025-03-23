dc6380a5342faf2598d19079b0a7857c20bf0a3f
Carlos <carlos.jose.rosales@estudiantat.upc.edu>
2025-03-23 17:24:31 +0100

    prova 1

--- src/main.cpp
+++ src/main.cpp
@@ -15,181 +15,193 @@ struct pair_hash {
 
 
 int main() {
+    vector<int> kValues = {2, 4, 6, 8};
+    vector<int> numHashesValues = {50, 100, 200, 300};
+    vector<int> bandsValues = {10, 20, 50, 100};
+    vector<int> rowsValues = {2, 3, 4, 5, 6};
+    vector<double> simThresholdValues = {0.4, 0.6, 0.8};
 
-    //Get time stamp begining
-    auto Tstart = high_resolution_clock::now();
+    for (int ii = 0; ii < kValues.size(); ++ii) {
+        for (int jj = 0; jj < numHashesValues.size(); ++jj) {
+            for (int kk = 0; kk < bandsValues.size(); ++kk) {
+                for (int ll = 0; ll < rowsValues.size(); ++ll) {
+                    for (int mm = 0; mm < simThresholdValues.size(); ++mm) {
 
-    // Leemos todos los documentos
-    string docsDir = "./exp-docs";
-    vector<fs::path> docs = getDocs(docsDir);
-    int numOfDocs = docs.size();
+                        int K = kValues[ii];
+                        int numHashes = numHashesValues[jj];
+                        int bands = bandsValues[kk];
+                        int rows = rowsValues[ll];
+                        double simThreshold = simThresholdValues[mm];
 
-    auto Tread = high_resolution_clock::now();
-    /////////////////////////////// GENERACION DE SHINGLES ///////////////////////////////
+                        if (bands * rows != numHashes) continue;
 
-    int K = 5;
-    vector<ShingleSet> docsShingles(numOfDocs);
-    //Vector with size of documents and shingles
-    vector<int> sizeOfDocs(numOfDocs);
-    vector<int> sizeOfDocs2(numOfDocs);
-    vector<int> sizeOfDocs3(numOfDocs);
+                        //Get time stamp begining
+                        auto Tstart = high_resolution_clock::now();
 
-    for (int i = 0; i < numOfDocs; ++i) {
-        string docPath = docsDir + "/" + docs[i].string();
-        string docText = loadDocument(docPath);
-        generateShingles(docsShingles[i], docText, K);
-        //Save size of document
-        sizeOfDocs[i] = docText.size();
-        sizeOfDocs2[i] = docsShingles[i].size();
-    }
-    auto Tshingles = high_resolution_clock::now();
-    /////////////////////////////// GENERACION DE MINHASHES //////////////////////////////
+                        // Leemos todos los documentos
+                        string docsDir = "./exp-docs";
+                        vector<fs::path> docs = getDocs(docsDir);
+                        int numOfDocs = docs.size();
 
-    int numHashes = 200;
-    vector<vector<uint64_t>> docsSignatures(numOfDocs);
+                        auto Tread = high_resolution_clock::now();
+                        /////////////////////////////// GENERACION DE SHINGLES ///////////////////////////////
 
-    //Vector with size of documents after shingles
+                        vector<ShingleSet> docsShingles(numOfDocs);
+                        //Vector with size of documents and shingles
+                        vector<int> sizeOfDocs(numOfDocs);
+                        vector<int> sizeOfDocs2(numOfDocs);
+                        vector<int> sizeOfDocs3(numOfDocs);
+
+                        for (int i = 0; i < numOfDocs; ++i) {
+                            string docPath = docsDir + "/" + docs[i].string();
+                            string docText = loadDocument(docPath);
+                            generateShingles(docsShingles[i], docText, K);
+                            //Save size of document
+                            sizeOfDocs[i] = docText.size();
+                            sizeOfDocs2[i] = docsShingles[i].size();
+                        }
+                        auto Tshingles = high_resolution_clock::now();
+                        /////////////////////////////// GENERACION DE MINHASHES //////////////////////////////
+
+                        vector<vector<uint64_t>> docsSignatures(numOfDocs);
+
+                        //Vector with size of documents after shingles
+
+                        for (int i = 0; i < numOfDocs; ++i) {
+                            docsSignatures[i] = getMinhashSignature(docsShingles[i], numHashes);
+                            //cout << "Signature doc " << i << ": ";
+                            //for (auto val : docsSignatures[i]) cout << val << " ";
+                            //cout << endl;
+                            //Save size of documents minHash
+                            sizeOfDocs3[i] = docsSignatures[i].size();
+                        }
+                        auto TminHash = high_resolution_clock::now();
+
+                        //////////////////////////////////////// LSH /////////////////////////////////////////
+
+                        vector<pair<int, int>> candidatePairs = LSH(docsSignatures, bands, rows);
+
+                        //////////////////////////////////// ESTADISTICAS ////////////////////////////////////
+
+                        double recall = 0.0;
+                        double precision = 0.0;
+                        int falsePositives = 0;
+                        int falseNegatives = 0;
+                        double truePositives = 0;
+
+                        /// CALCULO DE LOS PARES VERDADEROS ///
+                        unordered_set<pair<int,int>, pair_hash> trueSimilarPairs;
+                        for (int i = 0; i < numOfDocs; ++i) {
+                            for (int j = i + 1; j < numOfDocs; ++j) {
+                                double sim = shinglesJaccardSimilarity(docsShingles[i], docsShingles[j]);
+                                //cout << "Sim between " << docs[i].stem() << " and " << docs[j].stem() << ": " << sim << endl;
+                                
+                                if (sim >= simThreshold) {
+                                    trueSimilarPairs.insert({i,j});
+                                }
+                            }
+                        }
+
+                        unordered_set<pair<int, int>, pair_hash> candidateSet(
+                            candidatePairs.begin(), candidatePairs.end()
+                        );
+                        
+                        // True Positives y False Positives
+                        for (const auto& pair : candidatePairs) {
+                            if (trueSimilarPairs.count(pair)) ++truePositives;
+                            else ++falsePositives;
+                        }
+                        
+                        // False Negatives: Pares verdaderos no en candidatos
+                        for (const auto& truePair : trueSimilarPairs) {
+                            if (!candidateSet.count(truePair)) ++falseNegatives;
+                        }
+                        
+                        // Cálculo de recall y precisión
+                        if (truePositives + falseNegatives > 0) {
+                            recall = truePositives / (truePositives + falseNegatives);
+                        }
+                        if (truePositives + falsePositives > 0) {
+                            precision = truePositives / (truePositives + falsePositives);
+                        }
+
+                        //Final Timestamp
+                        auto Tfinal = high_resolution_clock::now();
+
+                        ///////////////////////////////// CALCULO DE TIEMPOS /////////////////////////////////
+
+                        //Duration to read
+                        auto Dread = duration_cast<microseconds>(Tread - Tstart);
+
+                        //Duration to generate shingles
+                        auto Dshingles = duration_cast<microseconds>(Tshingles - Tread);
+
+                        //Duration to minHash
+                        auto DminHash = duration_cast<microseconds>(TminHash - Tshingles);
+
+                        //Duration to finish
+                        auto Dtotal = duration_cast<microseconds>(Tfinal - Tstart);
+
+                        ofstream dataFile;
+                        dataFile.open("dataFile.txt");
 
 
-    for (int i = 0; i < numOfDocs; ++i) {
-        docsSignatures[i] = getMinhashSignature(docsShingles[i], numHashes);
-        cout << "Signature doc " << i << ": ";
-        for (auto val : docsSignatures[i]) cout << val << " ";
-        cout << endl;
-        //Save size of documents minHash
-        sizeOfDocs3[i] = docsSignatures[i].size();
-    }
-    auto TminHash = high_resolution_clock::now();
+                        //Data
+                        dataFile << endl << "-------------Data-------------" << endl;
+                        dataFile << "Number of Files: " << numOfDocs << " files" << endl;
+                        for (int i = 0; i < numOfDocs; ++i) {
+                            //Print sizes of documents in each fase
+                            dataFile << "--- Sizes of Document " << i << " ---" << endl;
+                            dataFile << "Cleaned: " << sizeOfDocs[i] << " bytes   ";
+                            dataFile << "Shingles: " << sizeOfDocs2[i] * K << " bytes   ";
+                            dataFile << "MinHash: " << sizeOfDocs3[i] * sizeof(uint64_t) << " bytes" << endl;
+                        }
 
-    //////////////////////////////////////// LSH /////////////////////////////////////////
+                        //Time
+                        dataFile << endl << "-------------Time-------------" << endl;
+                        dataFile << "Time to read clean files: " << Dread.count() << " ms" << endl;
+                        dataFile << "Time to generate shingles: " << Dshingles.count() << " ms" << endl;
+                        dataFile << "Time to generate minHash: " << DminHash.count() << " ms" << endl;
+                        dataFile << "Total time processing: " << Dtotal.count() << " ms" << endl;
 
-    int bands = 50;
-    int rows = 4;
-    if (bands * rows != numHashes) {
-        cout << "Error: bands * rows != numHashes" << endl;
-        return 1;
-    }
-    vector<pair<int, int>> candidatePairs = LSH(docsSignatures, bands, rows);
+                        //Statistics
+                        dataFile << endl << "-------------Statistics-------------" << endl;
+                        dataFile << "True Positives: " << truePositives << endl;
+                        dataFile << "False Positives: " << falsePositives << endl;
+                        dataFile << "False Negatives: " << falseNegatives << endl;
+                        dataFile << "Precision: " << precision << endl;
+                        dataFile << "Recall: " << recall << endl;
 
-    //////////////////////////////////// ESTADISTICAS ////////////////////////////////////
+                        ofstream csvFile;
+                        csvFile.open("dataFile.csv");
 
-    double recall = 0.0;
-    double precision = 0.0;
-    int falsePositives = 0;
-    int falseNegatives = 0;
-    double truePositives = 0;
-    double simThreshold = 0.4;
+                        // Encabezado del CSV
+                        csvFile << "NumOfDocs\n";
+                        csvFile << numOfDocs << "\n\n";
 
-    /// CALCULO DE LOS PARES VERDADEROS ///
-    unordered_set<pair<int,int>, pair_hash> trueSimilarPairs;
-    for (int i = 0; i < numOfDocs; ++i) {
-        for (int j = i + 1; j < numOfDocs; ++j) {
-            double sim = shinglesJaccardSimilarity(docsShingles[i], docsShingles[j]);
-            cout << "Sim between " << i << " and " << j << ": " << sim << endl;
-            if (sim >= simThreshold) {
-                trueSimilarPairs.insert({i,j});
+                        csvFile << "Cleaned,Shingles,MinHash\n";
+                        for (int i = 0; i < numOfDocs; ++i) {
+                            csvFile << sizeOfDocs[i] << "," 
+                                    << sizeOfDocs2[i] * K << ","
+                                    << sizeOfDocs3[i] * sizeof(uint64_t) << "\n";
+                        }
+                        csvFile << "\n";
+
+                        csvFile << "ReadCleanFiles,GenerateShingles,GenerateMinHash,TotalProcessing\n";
+                        csvFile << Dread.count() << ","
+                                << Dshingles.count() << ","
+                                << DminHash.count() << ","
+                                << Dtotal.count() << "\n\n";
+
+                        csvFile << "TruePositives,FalsePositives,FalseNegatives,Precision,Recall\n";
+                        csvFile << truePositives << ","
+                                << falsePositives << ","
+                                << falseNegatives << ","
+                                << precision << ","
+                                << recall << "\n";
+                    }
+                }
             }
         }
     }
-
-    unordered_set<pair<int, int>, pair_hash> candidateSet(
-        candidatePairs.begin(), candidatePairs.end()
-    );
-    
-    // True Positives y False Positives
-    for (const auto& pair : candidatePairs) {
-        if (trueSimilarPairs.count(pair)) ++truePositives;
-        else ++falsePositives;
-    }
-    
-    // False Negatives: Pares verdaderos no en candidatos
-    for (const auto& truePair : trueSimilarPairs) {
-        if (!candidateSet.count(truePair)) ++falseNegatives;
-    }
-    
-    // Cálculo de recall y precisión
-    if (truePositives + falseNegatives > 0) {
-        recall = truePositives / (truePositives + falseNegatives);
-    }
-    if (truePositives + falsePositives > 0) {
-        precision = truePositives / (truePositives + falsePositives);
-    }
-
-    //Final Timestamp
-    auto Tfinal = high_resolution_clock::now();
-
-    
-    
-    ///////////////////////////////// CALCULO DE TIEMPOS /////////////////////////////////
-
-    //Duration to read
-    auto Dread = duration_cast<microseconds>(Tread - Tstart);
-
-    //Duration to generate shingles
-    auto Dshingles = duration_cast<microseconds>(Tshingles - Tread);
-
-    //Duration to minHash
-    auto DminHash = duration_cast<microseconds>(TminHash - Tshingles);
-
-    //Duration to finish
-    auto Dtotal = duration_cast<microseconds>(Tfinal - Tstart);
-
-    ofstream dataFile;
-    dataFile.open("dataFile.txt");
-
-
-    //Data
-    dataFile << endl << "-------------Data-------------" << endl;
-    dataFile << "Number of Files: " << numOfDocs << " files" << endl;
-    for (int i = 0; i < numOfDocs; ++i) {
-        //Print sizes of documents in each fase
-        dataFile << "--- Sizes of Document " << i << " ---" << endl;
-        dataFile << "Cleaned: " << sizeOfDocs[i] << " bytes   ";
-        dataFile << "Shingles: " << sizeOfDocs2[i] * K << " bytes   ";
-        dataFile << "MinHash: " << sizeOfDocs3[i] * sizeof(uint64_t) << " bytes" << endl;
-    }
-
-    //Time
-    dataFile << endl << "-------------Time-------------" << endl;
-    dataFile << "Time to read clean files: " << Dread.count() << " ms" << endl;
-    dataFile << "Time to generate shingles: " << Dshingles.count() << " ms" << endl;
-    dataFile << "Time to generate minHash: " << DminHash.count() << " ms" << endl;
-    dataFile << "Total time processing: " << Dtotal.count() << " ms" << endl;
-
-    //Statistics
-    dataFile << endl << "-------------Statistics-------------" << endl;
-    dataFile << "True Positives: " << truePositives << endl;
-    dataFile << "False Positives: " << falsePositives << endl;
-    dataFile << "False Negatives: " << falseNegatives << endl;
-    dataFile << "Precision: " << precision << endl;
-    dataFile << "Recall: " << recall << endl;
-
-    ofstream csvFile;
-    csvFile.open("dataFile.csv");
-
-    // Encabezado del CSV
-    csvFile << "NumOfDocs\n";
-    csvFile << numOfDocs << "\n\n";
-
-    csvFile << "Cleaned,Shingles,MinHash\n";
-    for (int i = 0; i < numOfDocs; ++i) {
-        csvFile << sizeOfDocs[i] << "," 
-                << sizeOfDocs2[i] * K << ","
-                << sizeOfDocs3[i] * sizeof(uint64_t) << "\n";
-    }
-    csvFile << "\n";
-
-    csvFile << "ReadCleanFiles,GenerateShingles,GenerateMinHash,TotalProcessing\n";
-    csvFile << Dread.count() << ","
-            << Dshingles.count() << ","
-            << DminHash.count() << ","
-            << Dtotal.count() << "\n\n";
-
-    csvFile << "TruePositives,FalsePositives,FalseNegatives,Precision,Recall\n";
-    csvFile << truePositives << ","
-            << falsePositives << ","
-            << falseNegatives << ","
-            << precision << ","
-            << recall << "\n";
-
 }
